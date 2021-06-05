a0e78cb833c56c26ab488a5dadd124173f90bb77
DHARSIKA S <83574724+DHARSIKA11@users.noreply.github.com>
2021-06-05 15:14:21 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Header() {
+  return (<header>
+    <h1>SHAPE AI</h1>
+  </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This was an amazing bootcamp from scratch taken by Shaurya Sinha Sir. We
+        really gained so much knowledge of Javascript,React js.
+      </p>
+    </div>
+  );
+}
+export default Note;
